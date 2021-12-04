4338bbe3b7aa2ff61ca5eacb3917ccf31c769e1f
Shark-vil <shark_vil@mail.ru>
2021-12-04 22:55:08 +0300

    Implementation of events in the main window

--- CustomCircleUserControl/MainWindow.xaml.cs
+++ CustomCircleUserControl/MainWindow.xaml.cs
@@ -1,28 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CustomCircleUserControl.UserControls;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CustomCircleUserControl
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private TransformControl _currentTransformControl;
+
         public MainWindow()
         {
             InitializeComponent();
+
+#if DEBUG
+            App.AllocConsole();
+#endif
+
+            Workingspace.MouseLeftButtonDown += Workingspace_MouseLeftButtonDown;
+
+            TransformControl.a_StartedTransform += (TransformControl transform) =>
+            {
+                if (_currentTransformControl != null && _currentTransformControl != transform)
+                    _currentTransformControl.StopTransform();
+
+                Workingspace.Children.Add(transform);
+                _currentTransformControl = transform;
+            };
+
+            TransformControl.a_StoppedTransform += (TransformControl transform) =>
+            {
+                Workingspace.Children.Remove(transform);
+                _currentTransformControl = null;
+            };
+        }
+
+        private void Workingspace_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_currentTransformControl == null)
+            {
+                var circleControl = new CircleControl();
+                circleControl.a_StartEditMode += () => Workingspace.Children.Remove(circleControl);
+                circleControl.a_StopEditMode += () => Workingspace.Children.Add(circleControl);
+
+                Canvas.SetLeft(circleControl, Workingspace.ActualWidth / 2 - circleControl.Width / 2);
+                Canvas.SetTop(circleControl, Workingspace.ActualHeight / 2 - circleControl.Height / 2);
+
+                circleControl.Edit();
+            }
+            else
+            {
+                _currentTransformControl.StopTransform();
+            }
         }
     }
 }
