d4d36b4fb21df24b4084d70bc473a2e85974af6c
Shark-vil <shark_vil@mail.ru>
2021-12-04 22:52:25 +0300

    Activating console for debug mode

--- CustomCircleUserControl/App.xaml.cs
+++ CustomCircleUserControl/App.xaml.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace CustomCircleUserControl
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+#if DEBUG
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+
+        [DllImport("Kernel32")]
+        public static extern void FreeConsole();
+#endif
     }
 }
